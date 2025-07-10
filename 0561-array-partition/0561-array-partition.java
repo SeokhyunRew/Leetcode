82605a85379178fe745c1c8407853fe8fad7483a
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-07-11 00:31:37 +0900

    Time: 6 ms (98.17%), Space: 46 MB (98.9%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,10 +1,28 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int answer=0;
-        
-        for(int i=0; i<nums.length; i+=2) answer+=nums[i];
+        nums = countingSort(nums);
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i += 2) answer += nums[i];
 
         return answer;
+    }
+
+    private int[] countingSort(int[] nums) {
+        int offset = 10000; 
+        int[] countArr = new int[20001]; 
+        int[] returnArr = new int[nums.length];
+        int index = 0;
+
+        for (int num : nums) countArr[num + offset]++;
+
+        for (int i = 0; i < countArr.length; i++) {
+            while (countArr[i] > 0) {
+                returnArr[index++] = i - offset; 
+                countArr[i]--;
+            }
+        }
+
+        return returnArr;
     }
 }
