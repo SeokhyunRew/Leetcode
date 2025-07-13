62cc56d6e9678e97ebc2f5ba73354ba02d7c003c
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-07-13 14:02:00 +0900

    Time: 7 ms (38.37%), Space: 56.2 MB (94.97%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,13 +11,15 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         List<Integer> arr = new ArrayList<>();
+        int size = 0;
 
         while (head != null) {
             arr.add(head.val);
             head = head.next;
+            size++;
         }
 
-        for(int i=0; i<arr.size()/2; i++) if(arr.get(i) != arr.get(arr.size()-i-1)) return false;
+        for(int i=0; i<size/2; i++) if(arr.get(i) != arr.get(size-i-1)) return false;
 
         return true;
     }
