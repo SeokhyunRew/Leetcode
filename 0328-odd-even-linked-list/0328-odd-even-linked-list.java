44c2ce61fe769e413638a7759b0c4c601eaa81d0
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-07-22 14:46:13 +0900

    Time: 0 ms (100%), Space: 44.8 MB (36.72%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,7 +10,9 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+        
         if (head == null || head.next == null) return head;
+
         ListNode oddHead = head;
         ListNode evenHead = head.next;
         ListNode evenCurr = evenHead;
