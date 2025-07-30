4da5b8bf775ac08c17fdd8ea7fa5885f7516dd28
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-07-30 15:39:21 +0900

    Time: 2 ms (36.63%), Space: 44.5 MB (63.57%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,29 +1,34 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        Set<Integer> used = new HashSet<>();
         List<List<Integer>> answer = new ArrayList<>();
-        
-        dfs(nums, answer, used, new ArrayList<>());
+        if(nums == null || nums.length == 0){
+            return answer;
+        }
+
+        List<Integer> list = Arrays.stream(nums)
+            .boxed()  
+            .collect(Collectors.toList());
+
+        dfs(answer,nums,list,new ArrayList<>());
 
         return answer;
     }
 
-    private void dfs(int[] nums, List<List<Integer>> answer, Set<Integer> used, List<Integer> curr){
-        
-        if(nums.length == used.size()){
-            answer.add(new ArrayList<>(curr));
+    private void dfs(List<List<Integer>> answer, int[] nums, List<Integer> toAdd,
+    List<Integer> added){
+
+        if(toAdd.isEmpty()){
+            answer.add(new ArrayList<>(added));
             return;
         }
 
-        for(int num : nums){
-            if(!used.contains(num)){
-                curr.add(num);
-                used.add(num);
-                dfs(nums, answer, used, curr);
-                curr.remove(curr.size()-1);
-                used.remove(num);
-            }
+        for(Integer i:toAdd){
+            List nextToAdd = new ArrayList<>(toAdd);
+            nextToAdd.remove(i);
+            added.add(i);
+            dfs(answer,nums,nextToAdd,added);
+            added.remove(i);
         }
+    
     }
-
 }
