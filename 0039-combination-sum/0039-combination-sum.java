1926bcea695b8fec86fe8b986210fb72fcb9b2ad
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-08-02 14:34:28 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+
+        dfs(answer, curr, 0, candidates, target);
+
+        return answer;
+    }
+
+    private static void dfs(List<List<Integer>> answer, List<Integer> curr, int beforeIndex, int[] candidates, int target){
+        
+        if(target==0){
+            answer.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i=beforeIndex; i<candidates.length; i++){
+            int currNum = candidates[i];
+            if(currNum>target) return;
+            curr.add(currNum);
+            dfs(answer, curr, i, candidates, target-currNum);
+            curr.remove(curr.size()-1);
+        }
+    }
+}
