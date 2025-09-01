c19dba516cbc88a9d8b2ce9c0f669f87dfbd4ad3
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-09-01 23:12:19 +0900

    Time: 56 ms (5.42%), Space: 56.4 MB (52.51%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,48 +1,41 @@
 class Solution {
-    Map<Integer, Integer> keep = new HashMap<>();
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
 
-        for(int[] arr : edges){
-            graph.putIfAbsent(arr[0], new ArrayList<>());
-            graph.putIfAbsent(arr[1], new ArrayList<>());
-
-            graph.get(arr[0]).add(arr[1]);
-            graph.get(arr[1]).add(arr[0]);
-        }
-
-        for(int key : graph.keySet()){
-            dfs(key, key, -1, 0, graph);
-        }
-
-        int min = Integer.MAX_VALUE;
-
-        for(Map.Entry<Integer, Integer> entry : keep.entrySet()){
-            if(entry.getValue()<min) min = entry.getValue();
-        }
-
-        List<Integer> answer = new ArrayList<>();
-
-        for(Map.Entry<Integer, Integer> entry : keep.entrySet()){
-            if(entry.getValue()==min) answer.add(entry.getKey());
-        }
-
-        if(n==1) answer.add(0);
+        if(n==1) return List.of(0);
         
-        return answer;
-    }
+        Map<Integer,List<Integer>> map = new HashMap<>();
 
-    private void dfs(int root, int start, int before, int depth, Map<Integer, List<Integer>> graph){
-        
-        if(graph.get(start).size()==1&&before==graph.get(start).get(0)){
-            if(keep.get(root)==null || keep.get(root)<depth) keep.put(root, depth);
-            return;
+        for(int[] arr:edges){
+            map.putIfAbsent(arr[0],new ArrayList<>());
+            map.putIfAbsent(arr[1],new ArrayList<>());
+            map.get(arr[0]).add(arr[1]);
+            map.get(arr[1]).add(arr[0]);
         }
 
-        for(int num : graph.get(start)){
-            if(num==before) continue;
-            dfs(root, num, start, depth+1, graph);
+        int size = map.size();
+        List<Integer> leafNodes = new ArrayList<>();
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            if (entry.getValue().size() == 1) {  
+                leafNodes.add(entry.getKey());
+            }
         }
 
+        while(size>2){
+            
+            List<Integer> newLeaves = new ArrayList<>();
+
+            size-=leafNodes.size();
+            for(int leaf:leafNodes){
+                int next = map.get(leaf).get(0);
+                map.get(next).remove((Object)leaf);
+                if(map.get(next).size() == 1){
+                    newLeaves.add(next);
+                }
+                leafNodes = newLeaves;
+            }
+
+            
+        }
+        return leafNodes;
     }
 }
